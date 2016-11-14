50c7784cccaa0b69c45d97d1d24dcea68d3923a1
deader <deader25@ukr.net>
2016-11-14 15:59:01 +0200

    After 2 lesson ES6

--- study/study.js
+++ study/study.js
@@ -24,17 +24,45 @@
 
 // Код 3
 
-Function.prototype.bind = function(context) {
-   var fn = this;
-   return function() {
-      return fn.apply(context, arguments);
-   };
-}
-var HelloPage = {
-   name: 'Habrahabr',
-   init: function() {
-      alert('Hello, ' + this.name);
-   }
-}
-//window.onload = HelloPage.init; //алертнул бы undefined, т.к. this указывало бы на window
-window.onload = HelloPage.init.bind(HelloPage); //вот теперь всё работает
+// Function.prototype.bind = function(context) {
+//    var fn = this;
+//    return function() {
+//       return fn.apply(context, arguments);
+//    };
+// }
+// var HelloPage = {
+//    name: 'Habrahabr',
+//    init: function() {
+//       alert('Hello, ' + this.name);
+//    }
+// }
+// //window.onload = HelloPage.init; //алертнул бы undefined, т.к. this указывало бы на window
+// window.onload = HelloPage.init.bind(HelloPage); //вот теперь всё работает
+
+// Код 4
+
+// var MyModule = {
+//    name: 'Habrahabr',
+//    sayPreved: function(name) {
+//       alert('PREVED ' + name.toUpperCase())
+//    },   
+//    sayPrevedToHabrahabr: function() {
+//       this.sayPreved(this.name);
+//    }
+// }
+// MyModule.sayPrevedToHabrahabr();
+
+// Код 5
+
+// var MyModule = (function() {
+//    var name = 'Habrahabr';
+//    function sayPreved() {
+//       alert('PREVED ' + name.toUpperCase());
+//    }
+//    return {
+//       sayPrevedToHabrahabr: function() {
+//          sayPreved(name);
+//       }
+//    }
+// })();
+// MyModule.sayPrevedToHabrahabr(); //alerts «PREVED Habrahabr»

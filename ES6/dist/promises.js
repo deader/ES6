793e44e575ceea9c7a9f1c8824c13a61c0363b62
deader <deader25@ukr.net>
2016-11-21 17:47:17 +0200

    After 15.2 lessons for ES6

---
+++ ES6/dist/promises.js
@@ -0,0 +1,38 @@
+'use strict';
+
+function applyForVisa(documents) {
+    console.log('Обработка заявления...');
+    var promise = new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            Math.random() > 0 ? resolve({}) : reject('В визе отказано: нехватило документов');
+            resolve();
+            reject();
+            resolve();
+        }, 2000);
+    });
+    return promise;
+}
+
+function getVisa(visa) {
+    console.info('Виза получена');
+    return new Promise(function (resolve, reject) {
+        setTimeout(function () {
+            return resolve(visa);
+        }, 2000);
+    });
+}
+
+function bookHotel(visa) {
+    console.log(visa);
+    console.log('Бронируем отель');
+    return Promise.resolve(visa);
+}
+
+function buyTickets(booking) {
+    console.log('Покупаем билеты');
+    console.log('Бронь', booking);
+}
+
+applyForVisa({}).then(getVisa).then(bookHotel).then(buyTickets).catch(function (error) {
+    return console.log(error);
+});
